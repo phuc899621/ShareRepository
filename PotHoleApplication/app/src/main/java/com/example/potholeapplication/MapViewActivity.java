22e1777f6d7d7a309512fdfa517dc331414c9ad3
phuc899621 <phuc899621@gmail.com>
2024-11-05 10:04:08 +0700

    phuc push

--- PotHoleApplication/app/src/main/java/com/example/potholeapplication/MapViewActivity.java
+++ PotHoleApplication/app/src/main/java/com/example/potholeapplication/MapViewActivity.java
@@ -1,6 +1,7 @@
 package com.example.potholeapplication;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MapViewActivity extends AppCompatActivity {
+import com.example.potholeapplication.databinding.ActivityMapViewBinding;
 
+public class MapViewActivity extends AppCompatActivity {
+    ActivityMapViewBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_map_view);
+        binding=ActivityMapViewBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setClickEvent();
+    }
+    public void setClickEvent(){
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
--- PotHoleApplication/app/src/main/java/com/example/potholeapplication/SettingActivity.java
+++ PotHoleApplication/app/src/main/java/com/example/potholeapplication/SettingActivity.java
@@ -1,6 +1,8 @@
 package com.example.potholeapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +25,40 @@ public class SettingActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setClickEvent();
+    }
+    public void setClickEvent(){
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        binding.cvYourPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SettingActivity.this,UserActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.tvUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SettingActivity.this,UserActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.tvLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //cái này sẽ xoa cac activity khác, quay lại nhu cũ
+                Intent intent = new Intent(SettingActivity.this, SplashScreenActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+
+            }
         });
     }
 }
