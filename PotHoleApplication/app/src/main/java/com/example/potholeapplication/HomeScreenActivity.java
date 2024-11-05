c9b4b1acdc632c8b4b4007fd9ea3bfc0839b79e8
phuc899621 <phuc899621@gmail.com>
2024-11-05 09:53:24 +0700

    phuc push

--- PotHoleApplication/app/src/main/java/com/example/potholeapplication/HomeScreenActivity.java
+++ PotHoleApplication/app/src/main/java/com/example/potholeapplication/HomeScreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.potholeapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.potholeapplication.databinding.ActivityHomeScreenBinding;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
@@ -18,20 +21,51 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import java.util.ArrayList;
 
 public class HomeScreenActivity extends AppCompatActivity {
-    LineChart lineChart;
+    LineChart lineChart;//chua dung den
+    ActivityHomeScreenBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_screen);
+        binding=ActivityHomeScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setClickEvent();// chỗ này cài su kien click
+
+    }
+    public void setClickEvent(){
+        binding.cvMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeScreenActivity.this,MapViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeScreenActivity.this,SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.userLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeScreenActivity.this,UserActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //chưa xong
+    public void chartData(){
         lineChart = findViewById(R.id.lineChart);
 
-        // Tạo dữ liệu cho LineChart
+
         ArrayList<Entry> entries = new ArrayList<>();
         entries.add(new Entry(0, 5));
         entries.add(new Entry(1, 10));
@@ -42,32 +76,20 @@ public class HomeScreenActivity extends AppCompatActivity {
         entries.add(new Entry(6, 6));
 
         LineDataSet lineDataSet = new LineDataSet(entries, "Performance");
-        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);  // Tạo đường cong
+        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
         lineDataSet.setCubicIntensity(0.2f);
         lineDataSet.setLineWidth(2f);
-        lineDataSet.setColor(getResources().getColor(R.color.purple));  // Màu cho phần tă
+        lineDataSet.setColor(getResources().getColor(R.color.purple));
         lineDataSet.setDrawFilled(true);
         lineDataSet.setFillColor(R.color.purple);
         lineDataSet.setFillAlpha(80);
 
-        lineDataSet.setDrawFilled(true); // Bật fill
-        lineDataSet.setFillDrawable(getResources().getDrawable(R.drawable.gradient_fill)); // Sử dụng gradient
-        LineData lineData = new LineData(lineDataSet);
-        lineChart.setData(lineData);;
-        lineChart.getAxisLeft().setEnabled(false);
-        lineChart.getAxisLeft().setDrawGridLines(false); // Tắt đường chỉ định trục Y trái
-        lineChart.getAxisRight().setDrawGridLines(false); // Tắt đường chỉ định trục Y phải
+
         lineChart.getXAxis().setDrawGridLines(false);
         lineChart.animateX(1000);
         final String[] daysOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
-        // Thiết lập trục X với nhãn tùy chỉnh
-        XAxis xAxis = lineChart.getXAxis();
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(daysOfWeek));
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setGranularity(1f); // Đảm bảo mỗi nhãn cách đều nhau
-        xAxis.setLabelCount(daysOfWeek.length); // Số lượng nhãn
-        // Tùy chỉnh biểu đồ
+
         lineChart.getDescription().setEnabled(false);
         lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
         lineChart.getAxisRight().setEnabled(false);
--- PotHoleApplication/app/src/main/java/com/example/potholeapplication/LoginScreenActivity.java
+++ PotHoleApplication/app/src/main/java/com/example/potholeapplication/LoginScreenActivity.java
@@ -1,6 +1,8 @@
 package com.example.potholeapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginScreenActivity extends AppCompatActivity {
+import com.example.potholeapplication.databinding.ActivityLoginScreenBinding;
 
+public class LoginScreenActivity extends AppCompatActivity {
+    ActivityLoginScreenBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_screen);
+        binding=ActivityLoginScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        setClickEvent(); //cài su kien click
+    }
+    public void setClickEvent(){
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginScreenActivity.this,HomeScreenActivity.class);
+                startActivity(intent);
+            }
+        });
+        binding.tvCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginScreenActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
