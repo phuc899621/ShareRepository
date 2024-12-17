ae2d80de33e1c117286ce6ada4c253f5b349d10b
Admin <quantranphu2003@gmail.com>
2024-12-17 21:03:33 +0700

    quan

--- PotHoleApplication/app/src/main/java/com/example/potholeapplication/RankingActivity.java
+++ PotHoleApplication/app/src/main/java/com/example/potholeapplication/RankingActivity.java
@@ -8,17 +8,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RankingActivity extends AppCompatActivity {
+import com.example.potholeapplication.class_pothole.RankingAdapter;
+import com.example.potholeapplication.databinding.ActivityRankingBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class RankingActivity extends AppCompatActivity {
+    ActivityRankingBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityRankingBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_ranking);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+        List <String> data = new ArrayList<>();
+        data.add("nam");
+        data.add("tuan");
+        RankingAdapter ranking = new RankingAdapter(data);
+        binding.recycleview.setAdapter(ranking);
     }
+
 }
---
+++ PotHoleApplication/app/src/main/java/com/example/potholeapplication/class_pothole/RankingAdapter.java
@@ -0,0 +1,47 @@
+package com.example.potholeapplication.class_pothole;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.potholeapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.ViewHolder>{
+    List<String> Data =new ArrayList<>();
+
+    public RankingAdapter(List<String> data) {
+        Data = data;
+    }
+
+    @NonNull
+    @Override
+    public RankingAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ranking_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RankingAdapter.ViewHolder holder, int position) {
+        holder.tvName.setText(Data.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return Data.size();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+        }
+    }
+
+}
